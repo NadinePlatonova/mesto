075765e1b65eabfbcc9241bccdba765619d86659
Nadezhda Platonova <platonova.nadezhda.webdev@yandex.ru>
2021-05-20 00:40:28 +0300

    feat: прописать открытие попапа в js

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+let openPopup = document.querySelector('.popup-open');
+let popup = document.querySelector('body > .popup');
+let closePopup = document.querySelector('.popup__close');
+
+openPopup.addEventListener('click', function() {
+  popup.classList.add('popup_opened');
+})
