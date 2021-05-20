9492dcb83b822e160e2c4468ef900fdf7da3182f
Nadezhda Platonova <platonova.nadezhda.webdev@yandex.ru>
2021-05-20 22:05:08 +0300

    feat: добавить закрытие попапа в js

--- scripts/script.js
+++ scripts/script.js
@@ -5,3 +5,7 @@ let closePopup = document.querySelector('.popup__close');
 openPopup.addEventListener('click', function() {
   popup.classList.add('popup_opened');
 })
+
+closePopup.addEventListener('click', function() {
+  popup.classList.remove('popup_opened');
+})
