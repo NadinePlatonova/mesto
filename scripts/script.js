9395e2bdabd486a1c6e1b4e8b357d67b03134323
Nadezhda Platonova <platonova.nadezhda.webdev@yandex.ru>
2021-05-22 00:04:52 +0300

    feat: добавить сохранение данных попапа в js

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,29 @@
-let openPopup = document.querySelector('.popup-open');
+let openPopup = document.querySelector('.profile__edit-button');
 let popup = document.querySelector('body > .popup');
 let closePopup = document.querySelector('.popup__close');
+let formElement = document.querySelector('.popup__container');
+let nameInput = document.querySelector('.popup__text_type_name');
+let jobInput = document.querySelector('.popup__text_type_role');
+let nameProfile = document.querySelector('.profile__name');
+let roleProfile = document.querySelector('.profile__role');
 
-openPopup.addEventListener('click', function() {
+function openPopupForm () {
+  nameInput.value = nameProfile.textContent
+  jobInput.value = roleProfile.textContent
   popup.classList.add('popup_opened');
-})
+}
 
-closePopup.addEventListener('click', function() {
+function closePopupForm () {
   popup.classList.remove('popup_opened');
-})
+}
+
+function formSubmitHandler (evt) {
+  evt.preventDefault();
+  nameProfile.textContent = nameInput.value
+  roleProfile.textContent = jobInput.value
+  closePopupForm();
+}
+
+openPopup.addEventListener('click', openPopupForm);
+closePopup.addEventListener('click', closePopupForm);
+formElement.addEventListener('submit', formSubmitHandler);
